e511247e48524436c7dbbb7d4a4b07f69986b0be
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-07-17 00:20:08 +0200

    Updates

---
+++ precache-manifest.808186467ca39f800261cf08a24ab502.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7ed5ff6b45ff4038c44eb1866457a3",
+    "url": "/harry/index.html"
+  },
+  {
+    "revision": "81249d76bd89d5861717",
+    "url": "/harry/static/css/main.b67817ca.chunk.css"
+  },
+  {
+    "revision": "925c6f07e3364f584867",
+    "url": "/harry/static/js/2.c84a9eea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/harry/static/js/2.c84a9eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81249d76bd89d5861717",
+    "url": "/harry/static/js/main.478d5719.chunk.js"
+  },
+  {
+    "revision": "ab4a999762b297f473fd",
+    "url": "/harry/static/js/runtime-main.26fbea1d.js"
+  },
+  {
+    "revision": "c5dad7d5ec51536a32a5caa244a75ff1",
+    "url": "/harry/static/media/harry-2.c5dad7d5.jpg"
+  },
+  {
+    "revision": "c770d3147b05324f24a809421c3d36c8",
+    "url": "/harry/static/media/harry-3.c770d314.jpg"
+  },
+  {
+    "revision": "00e118d8544de31334a617c4a4a5808d",
+    "url": "/harry/static/media/harry-4.00e118d8.jpg"
+  },
+  {
+    "revision": "c687c20770af355611eb61c6d14b8e30",
+    "url": "/harry/static/media/harry-5.c687c207.jpg"
+  },
+  {
+    "revision": "c05cbfaa07ad9b2c4c7fe6dd2ec6ae7c",
+    "url": "/harry/static/media/harry.c05cbfaa.jpg"
+  }
+]);
